b81a4c492d56a5d35594709caa2800541ddc4ef6
LEE YONGJUN <wooooooak@gmail.com>
2019-01-27 23:24:16 +0900

    document 에러 수정

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,36 +1,11 @@
 const path = require('path');
 const url = require('url');
 const { app, BrowserWindow } = require('electron');
-const { remote, ipcRenderer } = require('electron');
 require('./ipc/fileIpcInit');
 require('./ipc/treeIpcInit');
 require('./ipc/compareInit');
 
 let win;
-
-document.getElementById('menu-button').addEventListener('click', (event) => {
-	ipcRenderer.send('display-app-menu', {
-		x: event.x,
-		y: event.y
-	});
-});
-
-document.getElementById('minimize-button').addEventListener('click', () => {
-	remote.getCurrentWindow().minimize();
-});
-
-document.getElementById('min-max-button').addEventListener('click', () => {
-	const currentWindow = remote.getCurrentWindow();
-	if (currentWindow.isMaximized()) {
-		currentWindow.unmaximize();
-	} else {
-		currentWindow.maximize();
-	}
-});
-
-document.getElementById('close-button').addEventListener('click', () => {
-	remote.app.quit();
-});
 
 const createWindow = () => {
 	win = new BrowserWindow({
